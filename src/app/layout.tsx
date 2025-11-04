f6c3ff6720617de5400b8376971f16b1a38e1624
wasique@dynsimulation.com <wasique@dynsimulation.com>
2025-11-04 17:21:57 +0530

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
-//import localFont from "next/font/local"; //
+import Script from "next/script";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-//import StickyHeader from "./components/StickyHeader";
-
-
 
 // Montserrat (Google font)
 const montserrat = Montserrat({
@@ -14,7 +11,6 @@ const montserrat = Montserrat({
   variable: "--font-montserrat",
 });
 
-// 1. Define the font object
 export const metadata: Metadata = {
   title: "JETSYS DEFENCE",
   description: "",
@@ -22,12 +18,26 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.variable}  antialiased`}>{children}</body>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-P0Q4E9M3TL"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-P0Q4E9M3TL');
+          `}
+        </Script>
+      </head>
+
+      <body className={`${montserrat.variable} antialiased`}>{children}</body>
     </html>
   );
 }
